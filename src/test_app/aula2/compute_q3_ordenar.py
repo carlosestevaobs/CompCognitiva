06494c5136f496a96035fad2bae592549017c986
JefersonNsoares <34108133+JefersonNSoares@users.noreply.github.com>
2021-11-06 11:45:15 -0300

    Realizadas alterações para melhorias do padrão do projeto

---
+++ src/test_app/aula2/compute_q3_ordenar.py
@@ -0,0 +1,22 @@
+from utils import ordenar
+import argparse
+
+def main():    
+    ap  = argparse.ArgumentParser()
+    ap.add_argument('-lst', '--lista1',
+                    default = ['maça', 'laranja', 'abacate', 'pêra', 'limão'],
+                    help = 'Lista de Strings')
+    
+    ap.add_argument('-cond', '--condicao1',
+                    default = False,
+                    help = 'Valor booleando para ordenação')    
+    
+    args = vars(ap.parse_args())
+    lista = args['lista1']   
+    condicao = args['condicao1']  
+    
+    listaOrd = ordenar(lista, condicao)
+    print(listaOrd)
+  
+if __name__ == '__main__': 
+    main()
---
+++ src/test_app/aula2/compute_q6_limiarDist.py
@@ -0,0 +1,29 @@
+from utils import limiar
+import argparse
+
+def main():    
+    ap  = argparse.ArgumentParser()
+    ap.add_argument('-plv', '--palavra',
+                    default = 'uva',
+                    help = 'palavra a ser procurada')
+    
+    ap.add_argument('-lst', '--lista',
+                    default = ['abacate', 'pera', 'uva', 'banana', 
+                               'maçã' , 'repolho', 'uva', 'feijão', 
+                               'arroz'],
+                    help = 'Lista de Strings') 
+    
+    ap.add_argument('-lim', '--valorLimiar',
+                    default = 2,
+                    help = 'limiar')
+    
+    args = vars(ap.parse_args())
+    palavra = args['palavra']
+    lista = args['lista']       
+    valorLimiar = args['valorLimiar']
+    
+    lista2 = limiar(palavra, lista, valorLimiar) 
+    print(lista2)     
+  
+if __name__ == '__main__': 
+    main()
