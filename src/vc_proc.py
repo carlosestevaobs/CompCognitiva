a2adc92782bafa2c6e2731cf44a5bf0ff3f51d99
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2021-12-15 10:26:06 -0300

    Funções de pré-processamento

--- src/vc_proc.py
+++ src/vc_proc.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 15 08:27:46 2021
-
-@author: Estevao
-"""
-
 
 import cv2
 
@@ -14,29 +8,19 @@ def equalizeHistogram(url):
     equ = cv2.equalizeHist(gray)
     return equ
 
-# para teste de visualização de diferença
-#image = equalizeHistogram("../data_especular_crop/train_images/confluente/0.png")
-#cv2.imwrite('equalizada.png',image)
-
 def borderDetection(url):       
     img = cv2.imread(url)        
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
     sobely = cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5)       
     return sobelx + sobely  
-    
-#image = borderDetection("../data_especular_crop/train_images/confluente/0.png")
-#cv2.imwrite('borda.png',image)
-
 
 def Gaussian(url):       
     img = cv2.imread(url)        
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     blur = cv2.GaussianBlur(gray,(5,5),0)
     return blur  
-    
-#image = Gaussian("../data_especular_crop/train_images/confluente/0.png")
-#cv2.imwrite('blur.png',image)
+
 
 
 
