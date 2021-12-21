832abda10589135a3f927fe88f8aadc39b0c0fcf
JefersonNsoares <34108133+JefersonNSoares@users.noreply.github.com>
2021-12-20 22:03:53 -0300

    Adicionado treinamento cnn e VGG

---
+++ test_app/VC/compute_treinarCNN.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, '../../')
+
+from src.vc_proc import treinarCNN
+
+training_dir_1 = '../../data_especular_crop-equalize/test_images'
+validation_dir_1 = '../../data_especular_crop-equalize/train_images'
+test_dir_1 = '../../data_especular_crop-equalize/val_images'
+
+x, y = treinarCNN(training_dir_1, validation_dir_1, test_dir_1)
+print('Sensibilidade: ' + str(x))
+print('Precisão: ' + str(y))
